9eeb873388eb5681977d0a42a2611629c5d6aa91
Shane Brunson <brunson.ws@gmail.com>
2021-09-03 10:21:43 -0500

    delete branches with choices

--- src/delete-branches.ts
+++ src/delete-branches.ts
@@ -1,9 +1,30 @@
-import simpleGit from "simple-git";
-const git = simpleGit();
+import inquirer from "inquirer";
+import { git } from "./git";
+import { logger } from "./logger";
 
 export const deleteBranches = async () => {
-  const all = await git.branch(["-r"]);
-  const branches = await git.branchLocal();
-  console.log("list of all branches", all);
-  console.log("list of branches", branches);
+  const remoteBranches = (await git.branch(["-r"])).all.map((branchName) =>
+    branchName.replace("origin/", "")
+  );
+  const { all: localBranches, current } = await git.branchLocal();
+
+  const branchesToDelete = localBranches
+    .filter(
+      (branchName) =>
+        !remoteBranches.includes(branchName) && current !== branchName
+    )
+    .map((branchName) => ({ name: branchName, checked: true }));
+
+  const { branches } = await inquirer.prompt([
+    {
+      type: "checkbox",
+      choices: branchesToDelete,
+      name: "branches",
+      message: "Which branches would you like to delete?",
+    },
+  ]);
+
+  logger.info(`Deleting the following branches: ${branches.join(", ")}`);
+
+  git.deleteLocalBranches(branches);
 };
